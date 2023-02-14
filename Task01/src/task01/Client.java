38f96cf4263b1b80f2f2859731dc7c8631620def
mandyykmt <mandykoh_1995@hotmail.com>
2023-02-14 15:22:36 +0800

    3.22pm

--- Task01/src/task01/Client.java
+++ Task01/src/task01/Client.java
@@ -8,15 +8,11 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client {
     
     public static void main(String[] args) throws IOException {
-
-        // Variables
-
 
         // open a socket to connect to the server on port 5000
         Socket socket = new Socket("localhost", 5000);
@@ -31,6 +27,8 @@ public class Client {
         Scanner email = new Scanner(System.in); 
         String emailInput = email.next(); 
 
+        // to do : check for email format
+
         InputStream is = socket.getInputStream();
         BufferedInputStream bis = new BufferedInputStream(is); 
         ObjectInputStream ois = new ObjectInputStream(bis); 
@@ -40,14 +38,12 @@ public class Client {
         System.out.println(randomNumbers); 
         
         // to do : generate mean and standard deviation
-        
-        // pseudo numbers
-        double mean = 100.50;
-        double std = 20.0; 
-
         // String[] array = randomNumbers.split(","); 
         // ArrayList<Integer> result = new ArrayList<>(); 
         
+        // pseudo values for mean and standard deviation 
+        double mean = 100.50;
+        double std = 20.0;         
         System.out.println("The average of the generated numbers is: " + mean); 
         System.out.println("The standard deviation of the generated numbers is : " + std);
 
@@ -55,19 +51,17 @@ public class Client {
         BufferedOutputStream bos = new BufferedOutputStream(os);
         ObjectOutputStream oos = new ObjectOutputStream(bos); 
 
-        
+        oos.writeUTF(nameInput);
+        oos.writeUTF(emailInput);
+        oos.writeFloat((float) mean);
+        oos.writeFloat((float) std); 
 
-        // nameOutput = oos.writeUTF(nameInput);
-        // emailOutput = oos.writeUTF(emailInput);
-        // float meanOutput = oos.writeFloat();
-        // float stdOutput = oos.writeFloat();        
+        System.out.println("Received name: " + nameInput);
+        System.out.println("Received email: " + emailInput);
+        System.out.println("Received mean: " + mean);
+        System.out.println("Received standard deviation: " + std);
 
-        // System.out.print("Received name: " + nameOutput);
-        // System.out.print("Received email: " + emailOutput);
-        // System.out.print("Received mean: " + meanOutput);
-        // System.out.print("Received standard deviation: " + stdOutput);
-
-        //dos.flush(); 
+        oos.flush(); 
                 
         oos.close();
         bos.close();
